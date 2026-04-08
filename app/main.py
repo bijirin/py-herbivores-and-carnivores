24d187c5ff4215d385ecaef905ff052a6e86f73a
bijirin <tsutrisn@gmail.com>
2026-04-08 22:23:19 +0700

    Fixing formatting per flake8 tests

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health=100, hidden=False):
+    def __init__(
+            self, name: str, 
+            health: int = 100, 
+            hidden: bool = False
+    ) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         self.alive.append(self)
 
-    def __print__(self):
+    def __print__(self) -> str:
         return f"{self.name} (Health: {self.health}, Hidden: {self.hidden})"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         message = (
             f"{{Name: {self.name}, "
             f"Health: {self.health}, "
@@ -21,13 +25,13 @@ class Animal:
 
 class Herbivore(Animal):
 
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
 
-    def bite(self, other: Herbivore):
+    def bite(self, other: Herbivore) -> None:
         if other.hidden:
             return
         if other in self.alive and isinstance(other, Herbivore):
