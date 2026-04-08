e551847118528b54170ec0772f12cf058cec3d7a
bijirin <tsutrisn@gmail.com>
2026-04-08 22:15:16 +0700

    Implementing class Animal, Herbivore, Carnivore

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health=100, hidden=False):
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        self.alive.append(self)
+
+    def __print__(self):
+        return f"{self.name} (Health: {self.health}, Hidden: {self.hidden})"
+
+    def __repr__(self):
+        message = (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+        return message
+
+
+class Herbivore(Animal):
+
+    def hide(self):
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+
+    def bite(self, other: Herbivore):
+        if other.hidden:
+            return
+        if other in self.alive and isinstance(other, Herbivore):
+            other.health -= 50
+            if other.health <= 0:
+                self.alive.remove(other)
